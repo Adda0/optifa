6839a7fe566c58a7ab4ba7a5e958c25016fe5f9c
David Chocholatý <chocholaty.david@protonmail.com>
2021-08-12 09:59:41 +0000

    Fill legend to results files

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -78,25 +78,25 @@ while getopts :a:b:o:h o; do
     esac
 done
 
+
 # Create results file.
 if [ ! -f "$F_OUTPUT" ]; then
     touch "$F_OUTPUT";
-    cat "legend..." > "$F_OUTPUT" #! TODO Complete legend
+    echo "Larger automaton,Smaller automaton,L.states,L.initial,L.final,L.transitions,S.states,S.initial,S.final,S.transitions,ET.B.states,ET.B.final,ET.B.smt.mean,ET.B.smt.stddev,ET.B.smt.median,ET.B.smt.user,ET.B.smt.system,ET.B.smt.min,ET.B.smt.max,ET.L.smt.checked,ET.L.smt.processed,ET.L.smt.sat,ET.L.smt.false,ET.L.smt.skipped,ET.L.smt.l.lasso,ET.L.smt.s.lasso,ET.L.smt.states,ET.L.smt.final,ET.L.smt.mean,ET.L.smt.stddev,ET.L.smt.median,ET.L.smt.user,ET.L.smt.system,ET.L.smt.min,ET.L.smt.max,ET.L.smt_free.checked,ET.L.smt_free.processed,ET.L.smt_free.sat,ET.L.smt_free.false,ET.L.smt_free.skipped,ET.L.smt_free.l.lasso,ET.L.smt_free.s.lasso,ET.L.smt_free.states,ET.L.smt_free.final,ET.L.smt_free.mean,ET.L.smt_free.stddev,ET.L.smt_free.median,ET.L.smt_free.user,ET.L.smt_free.system,ET.L.smt_free.min,ET.L.smt_free.max,ET.P.forward.checked,ET.P.forward.processed,ET.P.forward.sat,ET.P.forward.false,ET.P.forward.skipped,ET.P.forward.states,ET.P.forward.final,ET.P.forward.mean,ET.P.forward.stddev,ET.P.forward.median,ET.P.forward.user,ET.P.forward.system,ET.P.forward.min,ET.P.forward.max,ET.P.backward.checked,ET.P.backward.processed,ET.P.backward.sat,ET.P.backward.false,ET.P.backward.skipped,ET.P.backward.states,ET.P.backward.final,ET.P.backward.mean,ET.P.backward.stddev,ET.P.backward.median,ET.P.backward.user,ET.P.backward.system,ET.P.backward.min,ET.P.backward.max,ET.P.no_lengths.checked,ET.P.no_lengths.processed,ET.P.no_lengths.sat,ET.P.no_lengths.false,ET.P.no_lengths.skipped,ET.P.no_lengths.states,ET.P.no_lengths.final,ET.P.no_lengths.mean,ET.P.no_lengths.stddev,ET.P.no_lengths.median,ET.P.no_lengths.user,ET.P.no_lengths.system,ET.P.no_lengths.min,ET.P.no_lengths.max,FP.B.states,FP.B.final,FP.L.smt.checked,FP.L.smt.processed,FP.L.smt.sat,FP.L.smt.false,FP.L.smt.skipped,FP.L.smt.l.lasso,FP.L.smt.s.lasso,FP.L.smt.states,FP.L.smt.final,FP.L.smt.mean,FP.L.smt.stddev,FP.L.smt.median,FP.L.smt.user,FP.L.smt.system,FP.L.smt.min,FP.L.smt.max,FP.L.smt_free.checked,FP.L.smt_free.processed,FP.L.smt_free.sat,FP.L.smt_free.false,FP.L.smt_free.skipped,FP.L.smt_free.l.lasso,FP.L.smt_free.s.lasso,FP.L.smt_free.states,FP.L.smt_free.final,FP.L.smt_free.mean,FP.L.smt_free.stddev,FP.L.smt_free.median,FP.L.smt_free.user,FP.L.smt_free.system,FP.L.smt_free.min,FP.L.smt_free.max,FP.P.forward.checked,FP.P.forward.processed,FP.P.forward.sat,FP.P.forward.false,FP.P.forward.skipped,FP.P.forward.states,FP.P.forward.final,FP.P.forward.mean,FP.P.forward.stddev,FP.P.forward.median,FP.P.forward.user,FP.P.forward.system,FP.P.forward.min,FP.P.forward.max,FP.P.backward.checked,FP.P.backward.processed,FP.P.backward.sat,FP.P.backward.false,FP.P.backward.skipped,FP.P.backward.states,FP.P.backward.final,FP.P.backward.mean,FP.P.backward.stddev,FP.P.backward.median,FP.P.backward.user,FP.P.backward.system,FP.P.backward.min,FP.P.backward.max,FP.P.no_lengths.checked,FP.P.no_lengths.processed,FP.P.no_lengths.sat,FP.P.no_lengths.false,FP.P.no_lengths.skipped,FP.P.no_lengths.states,FP.P.no_lengths.final,FP.P.no_lengths.mean,FP.P.no_lengths.stddev,FP.P.no_lengths.median,FP.P.no_lengths.user,FP.P.no_lengths.system,FP.P.no_lengths.min,FP.P.no_lengths.max," > "$F_OUTPUT" #! TODO Complete legend
 fi
 if [ ! -f "$F_TESTED_COMBINATIONS" ]; then
     touch "$F_TESTED_COMBINATIONS";
-    cat "legend..." > "$F_TESTED_COMBINATIONS" #! TODO Complete legend
+    echo "Larger automaton,Smaller automaton," > "$F_TESTED_COMBINATIONS"
 fi
 
 # Print basic information about initial automata.
-echo -n ""$F_FA_A_ORIG","$F_FA_B_ORIG"," | tee -a "$F_OUTPUT" "$F_TESTED_COMBINATIONS" >/dev/null
-echo "" >> "$F_TESTED_COMBINATIONS"
+python3 print_automata_paths.py "$F_FA_A_ORIG" "$F_FA_B_ORIG" | tee -a "$F_OUTPUT" "$F_TESTED_COMBINATIONS" >/dev/null;
 python3 print_automata_sizes.py "$F_FA_A_ORIG" "$F_FA_B_ORIG" >> "$F_OUTPUT";
 
 # Run every algorithm version:
 ## Emptiness tests:
 ### Basic algorithm:
-hyperfine "python3 -u generate_basic_product.py --break_when_final $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
+hyperfine "python3 generate_basic_product.py --break_when_final $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
 
 cat "$F_DATA_OUT" >> "$F_OUTPUT";
 echo -n "$(cut --complement -f 1 -d, "$F_TIME_CSV")" > "$F_TIME_CSV"
@@ -105,7 +105,7 @@ cat "$F_TIME_CSV" >> "$F_OUTPUT";
 
 ### Length abstraction:
 # - With SMT solver:
-hyperfine "python3 -u resolve_satisfiability_length_abstraction.py --smt --break_when_final $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
+hyperfine "python3 resolve_satisfiability_length_abstraction.py --smt --break_when_final $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
 
 cat "$F_DATA_OUT" >> "$F_OUTPUT";
 echo -n "$(cut --complement -f 1 -d, "$F_TIME_CSV")" > "$F_TIME_CSV"
@@ -113,7 +113,7 @@ echo -n "$(tail -n +2 "$F_TIME_CSV")" > "$F_TIME_CSV"
 cat "$F_TIME_CSV" >> "$F_OUTPUT";
 
 # - Without SMT solver:
-hyperfine "python3 -u resolve_satisfiability_length_abstraction.py --break_when_final $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
+hyperfine "python3 resolve_satisfiability_length_abstraction.py --break_when_final $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
 
 cat "$F_DATA_OUT" >> "$F_OUTPUT";
 echo -n "$(cut --complement -f 1 -d, "$F_TIME_CSV")" > "$F_TIME_CSV"
@@ -122,7 +122,7 @@ cat "$F_TIME_CSV" >> "$F_OUTPUT";
 
 ### Parikh image:
 # - Forward lengths computation:
-hyperfine "python3 -u resolve_satisfiability_parikh_image.py --forward_lengths --break_when_final $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
+hyperfine "python3 resolve_satisfiability_parikh_image.py --forward_lengths --break_when_final $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
 
 cat "$F_DATA_OUT" >> "$F_OUTPUT";
 echo -n "$(cut --complement -f 1 -d, "$F_TIME_CSV")" > "$F_TIME_CSV"
@@ -130,7 +130,7 @@ echo -n "$(tail -n +2 "$F_TIME_CSV")" > "$F_TIME_CSV"
 cat "$F_TIME_CSV" >> "$F_OUTPUT";
 
 # - Backward lengths computation:
-hyperfine "python3 -u resolve_satisfiability_parikh_image.py --break_when_final $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
+hyperfine "python3 resolve_satisfiability_parikh_image.py --break_when_final $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
 
 cat "$F_DATA_OUT" >> "$F_OUTPUT";
 echo -n "$(cut --complement -f 1 -d, "$F_TIME_CSV")" > "$F_TIME_CSV"
@@ -138,7 +138,7 @@ echo -n "$(tail -n +2 "$F_TIME_CSV")" > "$F_TIME_CSV"
 cat "$F_TIME_CSV" >> "$F_OUTPUT";
 
 # - Without legths computation:
-hyperfine "python3 -u resolve_satisfiability_parikh_image.py --no_z_constraints --break_when_final $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
+hyperfine "python3 resolve_satisfiability_parikh_image.py --no_z_constraints --break_when_final $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
 
 cat "$F_DATA_OUT" >> "$F_OUTPUT";
 echo -n "$(cut --complement -f 1 -d, "$F_TIME_CSV")" > "$F_TIME_CSV"
@@ -148,7 +148,7 @@ cat "$F_TIME_CSV" >> "$F_OUTPUT";
 
 ## Full product generation:
 ### Basic algorithm:
-hyperfine "python3 -u generate_basic_product.py $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
+hyperfine "python3 generate_basic_product.py $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
 
 cat "$F_DATA_OUT" >> "$F_OUTPUT";
 echo -n "$(cut --complement -f 1 -d, "$F_TIME_CSV")" > "$F_TIME_CSV"
@@ -157,7 +157,7 @@ cat "$F_TIME_CSV" >> "$F_OUTPUT";
 
 ### Length abstraction:
 # - With SMT solver:
-hyperfine "python3 -u resolve_satisfiability_length_abstraction.py --smt $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
+hyperfine "python3 resolve_satisfiability_length_abstraction.py --smt $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
 
 cat "$F_DATA_OUT" >> "$F_OUTPUT";
 echo -n "$(cut --complement -f 1 -d, "$F_TIME_CSV")" > "$F_TIME_CSV"
@@ -165,7 +165,7 @@ echo -n "$(tail -n +2 "$F_TIME_CSV")" > "$F_TIME_CSV"
 cat "$F_TIME_CSV" >> "$F_OUTPUT";
 
 # - Without SMT solver:
-hyperfine "python3 -u resolve_satisfiability_length_abstraction.py $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
+hyperfine "python3 resolve_satisfiability_length_abstraction.py $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
 
 cat "$F_DATA_OUT" >> "$F_OUTPUT";
 echo -n "$(cut --complement -f 1 -d, "$F_TIME_CSV")" > "$F_TIME_CSV"
@@ -174,7 +174,7 @@ cat "$F_TIME_CSV" >> "$F_OUTPUT";
 
 ### Parikh image:
 # - Forward lengths computation:
-hyperfine "python3 -u resolve_satisfiability_parikh_image.py --forward_lengths $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
+hyperfine "python3 resolve_satisfiability_parikh_image.py --forward_lengths $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
 
 cat "$F_DATA_OUT" >> "$F_OUTPUT";
 echo -n "$(cut --complement -f 1 -d, "$F_TIME_CSV")" > "$F_TIME_CSV"
@@ -182,7 +182,7 @@ echo -n "$(tail -n +2 "$F_TIME_CSV")" > "$F_TIME_CSV"
 cat "$F_TIME_CSV" >> "$F_OUTPUT";
 
 # - Backward lengths computation:
-hyperfine "python3 -u resolve_satisfiability_parikh_image.py $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
+hyperfine "python3 resolve_satisfiability_parikh_image.py $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
 
 cat "$F_DATA_OUT" >> "$F_OUTPUT";
 echo -n "$(cut --complement -f 1 -d, "$F_TIME_CSV")" > "$F_TIME_CSV"
@@ -190,7 +190,7 @@ echo -n "$(tail -n +2 "$F_TIME_CSV")" > "$F_TIME_CSV"
 cat "$F_TIME_CSV" >> "$F_OUTPUT";
 
 # - Without legths computation:
-hyperfine "python3 -u resolve_satisfiability_parikh_image.py --no_z_constraints $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
+hyperfine "python3 resolve_satisfiability_parikh_image.py --no_z_constraints $F_FA_A_ORIG $F_FA_B_ORIG > "$F_DATA_OUT"" --export-csv "$F_TIME_CSV" -u second -w 1 -r 2;
 
 cat "$F_DATA_OUT" >> "$F_OUTPUT";
 echo -n "$(cut --complement -f 1 -d, "$F_TIME_CSV")" > "$F_TIME_CSV"
