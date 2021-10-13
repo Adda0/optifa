38f115a38e5d50b20459bb0621b29a6b6cfdc88f
David Chocholatý <chocholaty.david@protonmail.com>
2021-10-13 12:22:34 +0200

    Allow unifying symbols when loading automata

--- src/load_automata.py
+++ src/load_automata.py
@@ -26,7 +26,11 @@ import pickle
 
 # Main script function.
 def main():
-    fa_a_orig, fa_b_orig, f_fa_a_loaded, f_fa_b_loaded = parse_args()  # Parse program arguments.
+    fa_a_orig, fa_b_orig, f_fa_a_loaded, f_fa_b_loaded, unify_symbols = parse_args()  # Parse program arguments.
+
+    if unify_symbols:
+        fa_a_orig.unify_transition_symbols(unify_symbols)
+        fa_b_orig.unify_transition_symbols(unify_symbols)
 
     # Store parsed automata objects to separate files.
     pickle.dump(fa_a_orig, f_fa_a_loaded)
@@ -35,18 +39,26 @@ def main():
     f_fa_a_loaded.close()
     f_fa_b_loaded.close()
 
+    fa_a_orig.print_automaton()
+    fa_b_orig.print_automaton()
+
 
 def parse_args():
     """Parse arguments using argparse."""
     arg_parser = argparse.ArgumentParser(description='Script to store parsed automata objects into separate files.')
     arg_parser.add_argument('-a', metavar='AUTOMATON_A', type=str,
-                    help='Automaton A to generate product from.')
+                    help='Automaton A to load.')
     arg_parser.add_argument('-b', metavar='AUTOMATON_B', type=str,
-                    help='Automaton B to generate product from.')
+                    help='Automaton B to load.')
     arg_parser.add_argument('--out_a', metavar='AUTOMATON_A_LOADED', type=argparse.FileType('wb'),
-                    help='Automaton A object file to generate product from.')
+                    help='Automaton A object file to store loaded automaton to.')
     arg_parser.add_argument('--out_b', metavar='AUTOMATON_B_LOADED', type=argparse.FileType('wb'),
-                    help='Automaton B object filet o generate product from.')
+                    help='Automaton B object file to store loaded automaton to.')
+    symbols_group = arg_parser.add_mutually_exclusive_group()
+    symbols_group.add_argument('--unify-symbols', '-u', nargs = '*', metavar = 'SYMBOL', type = str, default = [],
+                    help = "Symbols to unify.")
+    symbols_group.add_argument('--keep-symbols', '-k', nargs = '*', metavar = 'SYMBOL', type = str, default = [],
+                    help = "Symbols to keep.")
 
     # Test for '--help' argument.
     if '--help' in sys.argv or '-h' in sys.argv:
@@ -63,7 +75,14 @@ def parse_args():
     fa_a_orig = symboliclib.parse(args.a)
     fa_b_orig = symboliclib.parse(args.b)
 
-    return fa_a_orig, fa_b_orig, args.out_a, args.out_b
+    # Symbols to exclude.
+    unify_symbols = []
+    if args.unify_symbols:
+        unify_symbols = args.unify_symbols
+    elif args.keep_symbols:
+        unify_symbols = (fa_a_orig.alphabet.union(fa_b_orig.alphabet)).difference(args.keep_symbols)
+
+    return fa_a_orig, fa_b_orig, args.out_a, args.out_b, unify_symbols
 
 
 if __name__ == "__main__":
--- src/prepare_fa.py
+++ src/prepare_fa.py
@@ -20,12 +20,7 @@ def main():
     automaton_name = sys.argv[1]
 
     fa_a = symboliclib.parse(automaton_name)
-    #name = fa_a.get_automaton_name()
-    #print(name)
 
-    #fa_a.rename_automaton("test")
-    #name = fa_a.get_automaton_name()
-    #print(name)
     fa_a.unify_transition_symbols()
 
     fa_a = fa_a.simple_reduce()
@@ -33,18 +28,9 @@ def main():
     fa_a.print_automaton('tmp_automaton')
     #fa_a.print_automaton() #DEBUG
 
-    #fa_a.change_initial_state(fa_a.states)
-
-    #fa_a.print_automaton(automaton_name)
-
-
-
-
-
-
 if __name__ == "__main__":
     print("Starting prepare_fa.py.")  # DEBUG
     main()
     print("Ending prepare_fa.py.")  # DEBUG
 
-# End of file prepare_fa.py #
+# End of file.
