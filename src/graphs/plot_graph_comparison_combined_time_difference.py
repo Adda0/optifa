38c883a6b893418c7043819d08033c90e64c00f0
David Chocholatý <chocholaty.david@protonmail.com>
2022-05-10 16:57:28 +0200

    Add graphs

--- src/graphs/plot_graph_comparison_combined_time_difference.py
+++ src/graphs/plot_graph_comparison_combined_time_difference.py
@@ -52,8 +52,8 @@ def get_dataframe(filename: str = "../../combined_results_appended.csv", verbose
 
     return df
 
-def plot_graph_scatter_comparison(df: pd.DataFrame, fig_location: str = None, show_figure: bool = False,
-                                  test_type: TEST_TYPE = TEST_TYPE.default()):
+
+def plot_graph_scatter_comparison(df: pd.DataFrame, fig_location: str = None, show_figure: bool = False):
     """
     Plot graph depicting scatter comparison of basic and length abstraction / Parikh image construction algorithms.
     Args:
@@ -62,19 +62,27 @@ def plot_graph_scatter_comparison(df: pd.DataFrame, fig_location: str = None, sh
         show_figure (bool): Whether to show the generated figure.
     """
 
+    df_et = df.filter(["ET.P.no_lengths.mean", "ET.C.mean"], axis=1)
+    df_et.rename(columns={'ET.P.no_lengths.mean': 'FP.P.no_lengths.mean', 'ET.C.mean': 'FP.C.mean'}, inplace=True)
+    df_fp = df.filter(["FP.P.no_lengths.mean", "FP.C.mean"], axis=1)
+    df_fp = pd.concat([df_fp, df_et])
+    df_fp.reset_index(inplace=True, drop=True)
+
+    #df_fp = df_fp[df_fp["FP.L.smt_free.mean"] <= df_fp[f"FP.L.smt.mean"]]
+
     sns.set_theme(style="whitegrid")
-    sbg = sns.relplot(x=f"Product Basic", y=f"Generated states of LAs + OA", data=df, legend=False)
+    sbg = sns.relplot(x=f"FP.P.no_lengths.mean", y=f"FP.C.mean", data=df_fp, legend=False)
     # sbg = sns.displot(x=f"{test_type.value}.B.states", y="count", hue="type", data=df, hue_order=hue_order, legend=True)
     # sbg.map_dataframe(sns.lineplot, f"{test_type.value}.B.states", f"{test_type.value}.B.states", color="grey", alpha=0.4)
     sbg.ax.axline(xy1=(0, 0), slope=1, color="grey", alpha=.4, dashes=(5, 2))
     # plt.yticks(df.loc[:, "count"].unique())
     # plt.xticks([i for i in range(0, df.loc[:, "count"].max())])
     # sbg.set_axis_labels("Čas od začátku směny (min)", "Počet rozvážejících řidičů")
-    max_val_axis = max(df[f"Product Basic"].max(), df[f"Generated states of LAs + OA"].max()) * 1.3
+    max_val_axis = max(df[f"FP.P.no_lengths.mean"].max(), df[f"FP.C.mean"].max()) * 1.3
     sbg.set(
-        xscale="log", yscale="log",
+        xscale="symlog", yscale="symlog",
         xlabel=None, ylabel=None,
-        xlim=(1, max_val_axis), ylim=(1, max_val_axis),
+        xlim=(-.1, max_val_axis), ylim=(-.1, max_val_axis),
     )
     sbg.despine()
     # sbg._legend.set_title("Druh vozidla")
@@ -93,14 +101,9 @@ if __name__ == "__main__":
     """
     Runs the main script operations when run as a standalone script.
     """
-    data_file_et = Path(sys.argv[1])
-    data_file_fp = Path(sys.argv[2])
+    data_file = Path(sys.argv[1])
     # print(data_file.stem)
     # df = get_dataframe(f"{filename}.csv", False)
-    df_et = get_dataframe(data_file_et, False)
-    df_fp = get_dataframe(data_file_fp, False)
+    df = get_dataframe(data_file, False)
     # plot_graph(df, fig_location=f"{data_file.stem}.png", show_figure=False)
-    plot_graph_scatter_comparison(df_et, fig_location=f"graph_la_pruning_et_scatter_modifications_lasso.pdf", show_figure=False,
-                                  test_type=TEST_TYPE.ET)
-    plot_graph_scatter_comparison(df_fp, fig_location=f"graph_la_pruning_fp_scatter_modifications_lasso.pdf", show_figure=False,
-                                  test_type=TEST_TYPE.FP)
+    plot_graph_scatter_comparison(df, fig_location=f"graph_combined_time_difference.pdf", show_figure=False)
